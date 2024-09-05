854138b4aacc0bb99a44367751ca8e6b435b154c
trofimovas <trofimovas@310D-3>
2024-09-05 15:30:42 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp2/CustomConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class CustomConverter
+    {
+
+
+        public void Convert(string input, out int result)
+        {
+            if (!int.TryParse(input, out result))
+            {
+                result = 0;
+                Console.WriteLine($"Cannot convert '{input}' to int.");
+            }
+        }
+
+
+        public void Convert(string input, out double result)
+        {
+            if (!double.TryParse(input, out result))
+            {
+                result = 0.0;
+                Console.WriteLine($"Cannot convert '{input}' to double.");
+            }
+        }
+    }
+}
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomConverter converter = new CustomConverter();
+
+            
+            Console.WriteLine("Введите число для конвертации в int:");
+            string inputInt = Console.ReadLine();
+            converter.Convert(inputInt, out int convertedInt);
+            Console.WriteLine($"Результат конвертации в int: {convertedInt}");
+
+            
+            Console.WriteLine("Введите число для конвертации в double:");
+            string inputDouble = Console.ReadLine();
+            converter.Convert(inputDouble, out double convertedDouble);
+            Console.WriteLine($"Результат конвертации в double: {convertedDouble}");
+        }
+    }
+}
